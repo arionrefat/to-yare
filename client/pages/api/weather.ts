110ae0c63f06ed97694bd678a3619351f18dd4a3
arionrefat <arionrefat@gmail.com>
2022-09-22 00:13:22 +0600

    added axios req

--- client/pages/api/weather.ts
+++
@@ -1,5 +0,0 @@
-import { apikey } from './../../config';
-
-export default function url(location: string[], unit: string): string {
-  return `https://api.openweathermap.org/data/2.5/weather?lat=${location[0]}&lon=${location[1]}&appid=${apikey}&units=${unit}`;
-}
