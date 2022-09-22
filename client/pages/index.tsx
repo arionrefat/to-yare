2b5fb496ab99959e993509ee873fdf6f20c90c88
arionrefat <arionrefat@gmail.com>
2022-09-22 16:23:58 +0600

    feat: added todolist

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,25 +1,35 @@
 import { apikey } from './../config';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
 
 function url(location: string[], unit: string): string {
   return `https://api.openweathermap.org/data/2.5/weather?lat=${location[0]}&lon=${location[1]}&appid=${apikey}&units=${unit}`;
 }
-console.log(url(['23.8245', '90.4056'], 'metric'))
 
 function WeatherApp() {
-  const [post, setPost] = useState(null);
+  const [temp, setTemp] = useState(null);
+  const [feelLike, setFeelsLike] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     axios.get(url(['23.8245', '90.4056'], 'metric')).then((response) => {
-      setPost(response.data.main.temp);
+      setTemp(response.data.main.temp);
+      setFeelsLike(response.data.main.feels_like);
+      setWeather(response.data.weather);
     });
   }, []);
 
-  if (!post) return null;
+  if (!temp) return null;
+
   return (
     <div>
-      <h1>{post}</h1>
+      <h1>{temp}</h1>
+      <h1>{feelLike}</h1>
+      {/* <h1>{weather}</h1> */}
+      <button>
+        <Link href='/todo'>Todo Page</Link>
+      </button>
     </div>
   );
 }
--- client/pages/todo/index.tsx
+++ client/pages/todo/index.tsx
@@ -1,3 +1,11 @@
-export default function () {
-  return <h1>he hehe</h1>;
+import TodoList from './components/TodoList';
+
+function Todos() {
+  return (
+    <div>
+      <TodoList items={['Learn React', 'Learn TypeScript']} />
+    </div>
+  );
 }
+
+export default Todos;
